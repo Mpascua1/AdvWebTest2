7b3800c1fd9a30ef7168d4686f317e5f5f976e3b
Mpascua1 <200218285@ub.edu.bz>
2021-10-13 15:32:18 -0600

    3rd

--- myjs/main2.js
+++ myjs/main2.js
@@ -1,21 +1,11 @@
 $('#cntus').hide();
 
+$('#us').hide();
+
 $('#cont').on('click', () => {
  $('#cntus').show(); 
 });
 
-$(document).ready(() => 
-{ 
-	//$('.product-card').show();
-	//let $navDropdown=$('.nav-menu');
-	//$navDropdown.show();
-	
-	const $menuButton = $('.menu-button');
-	const $navDropdown = $('.nav-menu');
-	
-	$($menuButton).on('click', () => {$navDropdown.show();});
-	
-	$navDropdown.on('mouseleave', () => {$navDropdown.hide();});
-
-	$('#login').on('click', () => { $loginForm.show(); });
+$('#signup').on('click', () => {
+ $('#us').show(); 
 });
